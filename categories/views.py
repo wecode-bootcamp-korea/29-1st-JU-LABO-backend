77c3ea98ee64924c5768fc0c2dfe1d7e821806d3
cch0807 <https://github.com/cch0807>
2022-01-27 17:42:30 +0900

    [modify] categoryview

--- categories/views.py
+++ categories/views.py
@@ -7,14 +7,13 @@ from .models                import Category, SubCategory, CategoryJoin
 
 class CategoryView(View):
   def get(self,request):
-    try:
-      subcategory = SubCategory.objects.all()
-      result = [i.name for i in subcategory]
 
+    sub_categories = [{
+      'id'  : sub_category.id,
+      'name': sub_category.name
+    } for sub_category in SubCategory.objects.all()]
 
-      return JsonResponse({'message': result}, status=200)
-    except:
-      return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+    return JsonResponse({'sub_categories': sub_categories}, status=200)
 
 class SubCategoryView(View):
   def get(self,request, category_id,subcategory_id):
@@ -33,7 +32,8 @@ class SubCategoryView(View):
           'categoryjoin': SubCategory.objects.get(id = i.categoryjoin.subcategory_id).name,
         })
 
-      return JsonResponse({'message': result}, status= 200)
+      return JsonResponse({'result': result}, status= 200)
+
     except:
       return JsonResponse({'message': 'KEY_ERROR'}, status=400) 
 
