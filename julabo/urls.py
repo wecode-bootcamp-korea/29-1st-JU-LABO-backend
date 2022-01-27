30c5ff34f7331021741fc85044a5fda2900aefe4
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2022-01-27 14:14:12 +0900

    [Add] add products list view

--- julabo/urls.py
+++ julabo/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from products.views import ProductDetailView
+
 urlpatterns = [
+    path('products', ProductDetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Product, Image
+
+class ProductDetailView(View):
+    def get(self, request):
+        if not Product.objects.filter(productgroup_id=2).exists():
+            return JsonResponse({'message':'productgroup_id error'}, status=400)
+
+        try:
+            products = Product.objects.filter(productgroup_id=2).order_by('is_default')
+
+            results = [
+                {
+                    'name'               : product.name,
+                    'category'           : product.categoryjoin.category.name,
+                    'subcategory'        : product.categoryjoin.subcategory.name,
+                    'ml'                 : product.ml,
+                    'price'              : product.price,
+                    'description'        : product.description,
+                    'image_urls'         : [image.image_url for image in Image.objects.filter(product_id=product.id)],
+                    'image_descriptions' : [
+                        image.image_url.split('/')[-1].split('.')[0] 
+                    for image in Image.objects.filter(product_id=product.id)
+                    ]
+                } for product in products
+            ]
+            return JsonResponse({'products': results}, status=200)
+        except Exception: # TODO: except 별로 작성하기
+            return JsonResponse({'message': 'fail'}, status=400)
