02fd24b7daf861be03ee24e393eaff28c8aa5aed
knuckles6974 <knuckles6974@gmail.com>
2022-01-27 11:10:27 +0900

    [ADD] users signup login

--- julabo/urls.py
+++ julabo/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 
 urlpatterns = [
+    path('/signup', SignUpView.as_view()),
+    path('/login',  LogInView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+import json, re , bcrypt, jwt 
 
-# Create your views here.
+from django.http            import JsonResponse
+from django.views           import View
+from django.core.exceptions import ValidationError
+
+from my_settings            import SECRET_KEY,ALGORITHM
+from users.models           import User
+
+
+REGEX_EMAIL = "^[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+$"
+REGEX_PASSWORD = "^(?=.{8,16}$)(?=.*[a-z])(?=.*[0-9]).*$"
+
+class SignUpView(View):
+    def post(self,request):
+        try:
+            data = json.loads(request.body)    
+            firstname          = data['first_name']
+            lastname           = data['last_name']
+            email              = data['email']
+            password           = data['password']
+
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message' : 'EMAIL_ALREADY_EXISTS'}, status=400)    
+            
+            if not re.match(REGEX_EMAIL , email):
+                return JsonResponse({"message" : "INVALID_EMAIL"}, status = 400)
+
+            if not re.match(REGEX_PASSWORD , password):
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
+
+            hashed_password = bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt()).decode('utf-8')
+            
+            User.objects.create(
+                first_name            = firstname,
+                last_name             = lastname,
+                email                 = email,
+                password              = hashed_password,
+                
+            )
+
+            return JsonResponse({'message':'SUCCESS'},status=200)
+    
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=401)
+
+class LogInView(View):
+    def post(self,request):
+        data = json.loads(request.body) 
+        try: 
+            if not User.objects.filter(
+                email    = data['email'],
+                password = data['password'],
+                ).exists():
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            return JsonResponse({'message':'SUCCESS'},status=200) 
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)            
+
+
+
+
