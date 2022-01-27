9c136c108e999c148ea8cb8fa68c052b192ecf31
cch0807 <https://github.com/cch0807>
2022-01-28 00:45:29 +0900

    [modify] category

--- categories/views.py
+++ categories/views.py
@@ -3,7 +3,7 @@ import json, csv
 from django.http            import JsonResponse
 from django.views           import View
 
-from .models                import Category, SubCategory, CategoryJoin
+from .models                import SubCategory
 
 class CategoryView(View):
   def get(self,request):
@@ -15,29 +15,5 @@ class CategoryView(View):
 
     return JsonResponse({'sub_categories': sub_categories}, status=200)
 
-class SubCategoryView(View):
-  def get(self,request, category_id,subcategory_id):
-    try:
-      if not CategoryJoin.objects.filter(category_id = category_id, subcategory_id = subcategory_id).exists():
-        return JsonResponse({'message':'INVALID_CATEGORY'}, status=400)
 
-      result = []
-      categoryjoin = CategoryJoin.objects.filter(category_id = category_id, subcategory_id = subcategory_id)[0].product_set.all()
-
-      for i in categoryjoin:
-        result.append({
-          'name'        : i.name,
-          'ml'          : i.ml,
-          'price'       : i.price,
-          'categoryjoin': SubCategory.objects.get(id = i.categoryjoin.subcategory_id).name,
-        })
-
-      return JsonResponse({'result': result}, status= 200)
-
-    except:
-      return JsonResponse({'message': 'KEY_ERROR'}, status=400) 
-
-class TypeView(View):
-  def get(self,request):
-    return JsonResponse({'message':'SUCCESS'}, status=200)
 
