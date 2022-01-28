5970224a106b3122b8b15373aa41e1e98cbc27c3
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2022-01-28 12:54:13 +0900

    [Fix]resolve conflicts

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path, include
+
+# from .views import ProductDetailView
+
+# urlpatterns = [
+#     path('/products/<int:productgroup_id>', ProductDetailView.as_view())
+# ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render
 
 # Create your views here.
