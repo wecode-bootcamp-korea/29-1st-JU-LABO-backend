17fb3023a150462535a6045ea2b8c26de7c2bd09
cch0807 <https://github.com/cch0807>
2022-01-28 16:04:50 +0900

    [modify] views

--- categories/views.py
+++ categories/views.py
@@ -1,31 +1,27 @@
-import json, csv
-
 from django.http            import JsonResponse
 from django.views           import View
 
-from .models                import Category, SubCategory, CategorySubCategory
+from products.models        import Product
 
 class ProductListView(View):
   def get(self,request):
     try:
-      category_id    = request.GET.get('category_id', None)
-      subcategory_id = request.GET.get('subcategory_id', None)
+      category_subcategory_id    = request.GET.get('category_subcategory_id', None)
 
-      if not CategorySubCategory.objects.filter(category_id = category_id, subcategory_id = subcategory_id).exists():
-        return JsonResponse({'message':'INVALID_CATEGORY'}, status=400)
+      products = Product.objects.filter(categoryjoin__id = category_subcategory_id)
 
-      Products = [{
+      products = [{
         'id'          : product.id,
         'name'        : product.name,
         'ml'          : product.ml,
         'price'       : product.price,
-        'category': {
-          'category_id'  : SubCategory.objects.get(id = product.categorysubcategory.subcategory_id).id,
-          'category_name': SubCategory.objects.get(id = product.categorysubcategory.subcategory_id).name
+        'subcategory': {
+          'subcategory_id'  : product.categorysubcategory.subcategory.id,
+          'subcategory_name': product.categorysubcategory.subcategory.name
         }
-      } for product in CategorySubCategory.objects.filter(category_id = category_id, subcategory_id = subcategory_id)[0].product_set.all()]
+      } for product in products]
 
-      return JsonResponse({'products': Products}, status= 200)
+      return JsonResponse({'products': products}, status= 200)
 
     except:
       return JsonResponse({'message': 'KEY_ERROR'}, status=400) 
