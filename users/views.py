9e1051c743ab26f99920b629eeb5450c91fbb893
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2022-01-31 16:35:43 +0900

    [Add] add cart list

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from itertools import product
 import json
+import datetime
 
 from django.views import View
 from django.http  import JsonResponse
@@ -15,17 +15,38 @@ class CartView(View):
         product_id = data['product_id']
         quantity = data['quantity']
 
-        cart, is_created = Cart.objects.get_or_create(user_id=user_id, product_id=product_id)
-        
-        if not is_created:
-            cart.quantity = F('quantity') + quantity
-        
-        else:
-            cart.quantity = quantity
-        
-        cart.save()
+        Cart.objects.update_or_create(
+            user_id=user_id, 
+            product_id=product_id, 
+            quantity=quantity
+            )
         
         return JsonResponse({'message': '등록성공'}, status=200)
 
-
+    def get(self, request):
+        # user_id = data['user_id']
+        # data = json.loads(request.body)
+        user_id = 1
+        # user_id = data['user_id']
         
+        items = Cart.objects.filter(user_id=user_id)
+        
+        results = [
+            {
+                'product_name' : item.product.name,
+                'quantity' : item.quantity,
+                'item_price' : item.product.price * item.quantity,
+                'subcategory' : item.product.categorysubcategory.subcategory.name,
+                'size' : item.product.ml
+            } 
+            for item in items]
+
+        total_price = 0
+        for result in results:
+            total_price += result['item_price']
+        
+        order_date = datetime.datetime.now()
+
+        return JsonResponse({'results': results, 
+                    'total_price': total_price, 
+                    'order_date': order_date}, status=200)
