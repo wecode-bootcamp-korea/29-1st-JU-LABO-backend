bd390fb6926d4053e89fa361e056717b6731745a
jiyeon.kim.ds <jiyeon.kim.ds@gmail.com>
2022-02-04 14:11:44 +0900

    [Modify] delete try/except

--- products/views.py
+++ products/views.py
@@ -8,26 +8,23 @@ class ProductGroupDetailView(View):
         if not Product.objects.filter(productgroup_id=productgroup_id).exists():
             return JsonResponse({'message':'productgroup_id error'}, status=400)
 
-        try:
-            products = Product.objects.filter(productgroup_id=productgroup_id).order_by('is_default')
+        products = Product.objects.filter(productgroup_id=productgroup_id).order_by('is_default')
 
-            results = [
-                {
-                    'name'               : product.name,
-                    'category'           : product.categorysubcategory.category.name,
-                    'subcategory'        : product.categorysubcategory.subcategory.name,
-                    'ml'                 : product.ml,
-                    'price'              : product.price,
-                    'description'        : product.description,
-                    'image_urls'         : [image.image_url for image in Image.objects.filter(product_id=product.id)],
-                    'image_descriptions' : [
-                        image.image_url.split('/')[-1].split('.')[0] 
-                    for image in Image.objects.filter(product_id=product.id)
-                    ]
-                } for product in products
-            ]
-            mls    = [result['ml'] for result in results]
-            prices = [result['price'] for result in results]
-            return JsonResponse({'products': results, 'mls': mls, 'prices': prices}, status=200)
-        except IndexError: # TODO: except 별로 작성하기
-            return JsonResponse({'message': 'Index Error'}, status=400)
+        results = [
+            {
+                'name'               : product.name,
+                'category'           : product.categorysubcategory.category.name,
+                'subcategory'        : product.categorysubcategory.subcategory.name,
+                'ml'                 : product.ml,
+                'price'              : product.price,
+                'description'        : product.description,
+                'image_urls'         : [image.image_url for image in Image.objects.filter(product_id=product.id)],
+                'image_descriptions' : [
+                    image.image_url.split('/')[-1].split('.')[0] 
+                for image in Image.objects.filter(product_id=product.id)
+                ]
+            } for product in products
+        ]
+        mls    = [result['ml'] for result in results]
+        prices = [result['price'] for result in results]
+        return JsonResponse({'products': results, 'mls': mls, 'prices': prices}, status=200)
