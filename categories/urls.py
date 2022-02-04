eb4cf2c84332ae63ef28ee45e826c3c8e8abf26a
knuckles6974 <knuckles6974@gmail.com>
2022-02-04 16:42:37 +0900

    [fix] category

--- categories/urls.py
+++ categories/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
 from .views      import ProductListView
+
 urlpatterns = {
-  path('/productview', ProductListView.as_view()),
+    path('/productview', ProductListView.as_view()),
+}
