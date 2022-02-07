4554c7e78e76f1002d3e254ff1708c4f877c64cc
cch0807 <https://github.com/cch0807>
2022-02-07 13:21:57 +0900

    [add]type

--- categories/urls.py
+++ categories/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views      import ProductListView, SubCategoriesView,ProductTypeView
+from .views      import ProductListView, SubCategoriesView
 urlpatterns = {
   path('/product', ProductListView.as_view()),
   path('/subcategory', SubCategoriesView.as_view()),
-  path('/type', ProductTypeView.as_view()),
 }
--- categories/views.py
+++ categories/views.py
@@ -18,20 +18,22 @@ class SubCategoriesView(View):
 class ProductListView(View):
   def get(self,request):
     try:
-      category_subcategory_id    = request.GET.get('category_subcategory_id', None)
+      category_subcategory_id = request.GET.get('category_subcategory_id', None)
+      type_ml                 = request.GET.get('ml', None)
+      image                   = Image.objects.all()
+      
+      if type_ml:
+        products = Product.objects.filter(categorysubcategory__id = category_subcategory_id, ml = type_ml)
+      else:
+        products = Product.objects.filter(categorysubcategory__id = category_subcategory_id) 
 
-      products = Product.objects.filter(categorysubcategory__id = category_subcategory_id)
-      image = Image.objects.all()
-      products = [{
-        'id'          : product.id,
-        'name'        : product.name,
-        'ml'          : product.ml,
-        'price'       : product.price,
-        'image':{
-          'img1': image.filter(product_id= product.id).values('image_url')[0],
-          'img2': image.filter(product_id= product.id).values('image_url')[1],
-          'img3': image.filter(product_id= product.id).values('image_url')[2]
-        },
+      products = [{   
+        'id'             : product.id,
+        'name'           : product.name,
+        'ml'             : product.ml,
+        'price'          : product.price,
+        'productgroup_id': product.productgroup.id,
+        'image'          : [img for img in image.filter(product_id = product.id).values('image_url')],
         'subcategory': {
           'subcategory_id'  : product.categorysubcategory.subcategory.id,
           'subcategory_name': product.categorysubcategory.subcategory.name
@@ -42,32 +44,3 @@ class ProductListView(View):
 
     except:
       return JsonResponse({'message': 'KEY_ERROR'}, status=400) 
-
-class ProductTypeView(View):
-  def get(self, request):
-    try:
-      category_subcategory_id    = request.GET.get('category_subcategory_id', None)
-      type_ml                    = request.GET.get('ml', None)
-
-      products = Product.objects.filter(categorysubcategory__id = category_subcategory_id, ml = type_ml)
-      image = Image.objects.all()
-
-      products = [{
-        'id'          : product.id,
-        'name'        : product.name,
-        'ml'          : product.ml,
-        'price'       : product.price,
-        'image':{
-          'img1': image.filter(product_id= product.id).values('image_url')[0],
-          'img2': image.filter(product_id= product.id).values('image_url')[1],
-          'img3': image.filter(product_id= product.id).values('image_url')[2]
-        },
-        'subcategory': {
-          'subcategory_id'  : product.categorysubcategory.subcategory.id,
-          'subcategory_name': product.categorysubcategory.subcategory.name
-        }
-      } for product in products]
-
-      return JsonResponse({'message': products}, status= 200)
-    except:
-      return JsonResponse({'message': 'KEY_ERROR'}, status=400)
