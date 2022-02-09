cbcdead49c092e8a8114fe8ff68ac10658b28ca5
knuckles6974 <knuckles6974@gmail.com>
2022-02-09 11:33:28 +0900

    [fix] login

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,11 @@
+import json 
+
 from django.http            import JsonResponse
 from django.views           import View
 
 from .models                import SubCategory
+from users.models           import User
+from products.models        import Product
 
 class SubCategoriesView(View):
   def get(self,request):
@@ -12,3 +16,6 @@ class SubCategoriesView(View):
     } for sub_category in SubCategory.objects.all()]
 
     return JsonResponse({'sub_categories': sub_categories}, status=200)
+
+
+
